759e9cae46ea5af94b46708e356714d9cb328899
sunakan <sunakan03@gmail.com>
2019-11-14 20:26:52 +0900

    [Add] ymlファイルの追加とそれ(yml)を読み込むための設定を記述

--- gatsby-sunabako/gatsby-config.js
+++ gatsby-sunabako/gatsby-config.js
@@ -13,6 +13,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `works`,
+        path: `${__dirname}/src/works.yaml`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
