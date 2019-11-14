f0aa2254c4ae87d5111ec652206c628ea171370d
sunakan <sunakan03@gmail.com>
2019-11-14 21:12:23 +0900

    [Add] Gatsbyの既存の挙動を変更(JSONにするところまで)

--- gatsby-sunabako/gatsby-node.js
+++ gatsby-sunabako/gatsby-node.js
@@ -1,7 +1,18 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+// slugをとってくる
+// then -> JSON
+exports.createPages = ({ graphql, actions}) => {
+  return graphql(`
+    {
+      allWorksYaml {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `).then(result => {
+    // stringifyの引数はインデント幅
+    console.log(JSON.stringify(result, null, 4))
+  })
+}
