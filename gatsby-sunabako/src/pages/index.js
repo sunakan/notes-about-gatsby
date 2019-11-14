da389b19501919c399f19bd59b4f93ee492461f1
sunakan <sunakan03@gmail.com>
2019-11-14 20:48:32 +0900

    [Add] 実際にGatsbyでYamlを読み込んで、GraphQLで取得して、描画するまで

--- gatsby-sunabako/src/pages/index.js
+++ gatsby-sunabako/src/pages/index.js
@@ -1,21 +1,35 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>こんにちは世界</h1>
+    {data.allWorksYaml.edges.map(edge => {
+      const work = edge.node
+      return <div>
+        {work.title} - {work.category} - {work.year}
+      </div>
+    })}
     <Link to="/about/">Go to about page</Link>
   </Layout>
 )
 
+export const query = graphql`
+  query {
+    allWorksYaml {
+      edges {
+        node {
+          title
+          category
+          year
+        }
+      }
+    }
+  }
+`
 export default IndexPage
