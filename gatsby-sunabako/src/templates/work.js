ec91771af8108ed3bd28e3b58535a0b172dcf555
sunakan <sunakan03@gmail.com>
2019-11-16 12:17:33 +0900

    [Add] GraphQL + YAML読み込み + 動的ルーティング

--- gatsby-sunabako/src/templates/work.js
+++ gatsby-sunabako/src/templates/work.js
@@ -1,16 +1,31 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 // コンポーネントからexporされているものを持ってくる
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const WorkPage = () => (
+const WorkPage = ({ data }) => (
   <Layout>
     <SEO title="About" />
-    <h1>This is work ペーーーージ</h1>
+    <h1>{data.worksYaml.title}</h1>
+    <div>{data.worksYaml.description}</div>
+    <div>
+      {data.worksYaml.category} - {data.worksYaml.year}
+    </div>
     <Link to="/">Topへ戻る</Link>
   </Layout>
 )
+
+export const query = graphql`
+  query($slug: String!) {
+    worksYaml(slug: { eq: $slug}) {
+      title
+      description
+      category
+      year
+    }
+  }
+`
 
 export default WorkPage
